7b4af3f299fc9e6d908e9288839462c1c212b1c2
Pozor <56485650+pozor22@users.noreply.github.com>
2023-06-10 13:28:40 +0300

    version 1.3.0

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,12 +1,24 @@
 from rest_framework import serializers
 
-from .models import Subscription
+from .models import Subscription, Plan
+
+
+class PlanSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = ('__all__')
 
 
 class SubscriptionSerializers(serializers.ModelSerializer):
+    plan = PlanSerializers()
+
     client_name = serializers.CharField(source="client.company_name")
     email = serializers.CharField(source="client.user.email")
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return instance.price
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
